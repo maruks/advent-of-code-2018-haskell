f749d022533aaedbdc31118fa125d6e5573ce27e
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-12-04 22:45:31 +0000

    day 4 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Day4
 
 import Text.Regex.PCRE
 
@@ -26,14 +27,36 @@ day3 = do
   file <- readFile "./test/day3.txt"
   let rs = map parseLine $ lines file where
         parseLine :: String -> Day3.Rectangle
-        parseLine s = let stringResult = s =~ "\\d+" :: AllTextMatches [] String
-                          [i, x, y, w, h] = map read $ getAllTextMatches stringResult :: [Int]
+        parseLine s = let result = s =~ "\\d+" :: AllTextMatches [] String
+                          [i, x, y, w, h] = map read $ getAllTextMatches result :: [Int]
                       in Day3.Rectangle i x y w h
   print $ Day3.solution1 rs
   print $ Day3.solution2 rs
+
+parseDay4Input :: String -> Day4.Log
+parseDay4Input s =
+  let result1 = s =~ "\\d+" :: AllTextMatches [] String
+      (_:month:day:hour:minute:more) = map read $ getAllTextMatches result1 :: [Int]
+      timestamp = 1440 * 31 * month + 1440 * day + 60 * hour + minute
+      result2 = s =~ "begins|wakes|falls" :: AllTextMatches [] String
+      logType = head $ getAllTextMatches result2
+  in
+    case logType of
+      "begins" -> Day4.BeginShift timestamp (head more)
+      "falls" -> Day4.FallAsleep timestamp minute
+      "wakes" -> Day4.WakeUp timestamp minute
+      _ -> error "invalid log entry"
+
+day4 :: IO ()
+day4 = do
+  file <- readFile "./test/day4.txt"
+  let rs = Day4.sortLog $ map parseDay4Input $ lines file
+  print $ Day4.solution1 rs
+  print $ Day4.solution2 rs
 
 main :: IO ()
 main = do
   day1
   day2
   day3
+  day4
--- src/Day4.hs
+++ src/Day4.hs
@@ -1,11 +1,13 @@
 module Day4
   ( solution1
+  , solution2
   , Log(..)
+  , sortLog
   ) where
 
-import qualified Data.List as List
-import qualified Data.Map.Strict as Map
-import qualified Data.Maybe as Maybe
+import Data.List as List
+import Data.Map.Strict as Map
+import Data.Maybe as Maybe
 
 type Timestamp = Int
 type GuardId = Int
@@ -14,36 +16,73 @@ type Minute = Int
 
 data Log
   = BeginShift Timestamp GuardId
-  | FallsAsleep Timestamp Minute
-  | WakesUp Timestamp Minute
+  | FallAsleep Timestamp Minute
+  | WakeUp Timestamp Minute
   deriving (Eq, Show)
 
--- [1518-10-01 00:00] Guard #3491 begins shift
--- [1518-08-31 00:47] wakes up
--- [1518-03-30 00:25] falls asleep
-
 solution1 :: [Log] -> Int
-solution1 xs = let stats = guardStats xs 0 Nothing Map.empty
-                   (guard, minutes) = sleepyGuard stats
-                   m = findMinute minutes (0,0)
-               in guard * m
+solution1 xs =
+  let stats = guardStats xs 0 Nothing Map.empty
+      (guard, minutes) = sleepyGuard stats
+      minute = findMinute $ sort minutes
+  in guard * minute
 
-sleepyGuard :: Map.Map GuardId [Minute] -> (GuardId, [Minute])
-sleepyGuard = Map.foldlWithKey (\a@(i,s) k v -> if length v > length s then (k,v) else a) (-1,[])
+sleepyGuard :: Map GuardId [Minute] -> (GuardId, [Minute])
+sleepyGuard =
+  foldlWithKey
+    (\acc@(_, minutes) guard minutes' ->
+        if length minutes' > length minutes
+          then (guard, minutes')
+          else acc)
+    (0, [])
 
-findMinute :: [Minute] -> (Int, Minute) -> Minute
-findMinute [] (_, m) = m
-findMinute all@(x:xs) acc@(s,m) = let elems = List.takeWhile (==x) all
-                                      size = length elems
-                                      newAcc = if size > s then (size, x) else acc
-                                      newXs = dropWhile (==x) all
-                                  in findMinute newXs newAcc
+findMinute' :: [Minute] -> (Int, Minute) -> (Int, Minute)
+findMinute' [] acc = acc
+findMinute' all@(x:xs) acc@(count, _) =
+  let elems = takeWhile (== x) xs
+      count' = 1 + length elems
+      newAcc =
+        if count' > count
+          then (count', x)
+          else acc
+      newXs = dropWhile (== x) xs
+  in findMinute' newXs newAcc
 
-guardStats :: [Log] -> GuardId -> Maybe Minute -> Map.Map GuardId [Minute] -> Map.Map GuardId [Minute]
+findMinute :: [Minute] -> Minute
+findMinute xs = snd $ findMinute' xs (0, 0)
+
+guardStats :: [Log]
+           -> GuardId
+           -> Maybe Minute
+           -> Map GuardId [Minute]
+           -> Map GuardId [Minute]
 guardStats [] _ _ acc = acc
-guardStats (x:xs) g ms acc =
+guardStats (x:xs) guard maybeMinuteFellAsleep acc =
   case x of
-    BeginShift _ newId -> guardStats xs newId Nothing acc
-    FallsAsleep _ m -> guardStats xs g (Just m) acc
-    WakesUp _ m -> guardStats xs g Nothing newAcc
-      where newAcc = Map.insertWith (++) g [Maybe.fromJust ms .. m - 1] acc
+    BeginShift _ newGuard -> guardStats xs newGuard Nothing acc
+    FallAsleep _ minute -> guardStats xs guard (Just minute) acc
+    WakeUp _ minute -> guardStats xs guard Nothing newAcc
+      where newAcc = Map.insertWith (++) guard [Maybe.fromJust maybeMinuteFellAsleep .. minute - 1] acc
+
+sortLog :: [Log] -> [Log]
+sortLog = sortOn timestamp
+  where
+    timestamp :: Log -> Timestamp
+    timestamp (BeginShift t _) = t
+    timestamp (FallAsleep t _) = t
+    timestamp (WakeUp t _) = t
+
+solution2 :: [Log] -> Int
+solution2 xs =
+  let stats = guardStats xs 0 Nothing Map.empty
+      (guard, _, minute) = mostFrequentlyAsleep stats
+  in guard * minute
+
+mostFrequentlyAsleep :: Map GuardId [Minute] -> (GuardId, Int, Minute)
+mostFrequentlyAsleep = foldlWithKey foldFn (0, 0, 0)
+  where
+    foldFn acc@(guard, count, minute) guard' minutes =
+      let (count', minute') = findMinute' (sort minutes) (0, 0)
+      in if count' > count
+           then (guard', count', minute')
+           else acc
