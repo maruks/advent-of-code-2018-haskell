185cd46074ecfb153ec609d3452f14abf9f377f1
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-12-09 18:39:51 +0000

    day8

--- src/Day8.hs
+++ src/Day8.hs
@@ -12,23 +12,20 @@ import Data.Set as Set
 solution1 :: [Int] -> Int
 solution1 = List.sum . fst . metadata
 
-xs ::[Int]
-xs = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
-
 metadata :: [Int] -> ([Int], Int)
-metadata all@(children:meta:xs)
-  | children == 0 = (List.take meta xs, 2 + meta )
+metadata (children:meta:xs)
+  | children == 0 = (List.take meta xs, 2 + meta)
   | otherwise = loop xs children [] 0 where
       loop ys 0 metas sum = (List.take meta (List.drop sum xs) ++ metas, sum + meta + 2)
-      loop ys c metas sum = let (ms, len ) = metadata ys
+      loop ys c metas sum = let (ms, len) = metadata ys
                             in loop (List.drop len ys) (c - 1) (metas ++ ms) (sum + len)
 
 solution2 :: [Int] -> Int
 solution2 = fst . value
 
 value :: [Int] -> (Int, Int)
-value all@(children:meta:xs)
-  | children == 0 = (List.sum $ List.take meta xs, 2 + meta )
+value (children:meta:xs)
+  | children == 0 = (List.sum $ List.take meta xs, 2 + meta)
   | otherwise = loop xs children Map.empty 1 0 where
       loop ys 0 valMap idx sum = (List.foldl' (\a e -> a + Maybe.fromMaybe 0 (valMap !? e)) 0 $ List.take meta (List.drop sum xs), sum + meta + 2)
       loop ys c valMap idx sum = let (val, len) = value ys
